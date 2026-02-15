a466723c5849a19615f47d721a950b232b86204b
Kateryna <katekirshe95@gmail.com>
2026-02-15 20:26:37 +0200

    fix

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -19,10 +19,15 @@ return axios.get(BASE_URL, { params: {
               message: 'Sorry, there are no images matching your search query. Please try again!',
               position: 'topRight'
           });
+        return
       }; 
       return response.data;
   })
   .catch(function (error) {
     console.log(error);
+    iziToast.error({ 
+            message: "API error.",
+            position: 'topRight'
+        })
   })
 }
--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
-import { getImagesByQuery } from "./js/pixabay-api"
-import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions"
-
-
-// createGallery()
-// clearGallery()
-// showLoader()
-// hideLoader()
+import { getImagesByQuery } from "./js/pixabay-api";
+import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-functions";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
 const form = document.querySelector(".form");
 form.addEventListener("submit", (event) => {
     event.preventDefault();
     const value = event.target.children[0].children[0].value;
     if (!value.trim().length) {
-        return alert("Поле порожнє!!!");
+        iziToast.error({ 
+            message: "The field can't be empty!",
+            position: 'topRight'
+        })
+        return 
     }
     clearGallery();
     showLoader();
     getImagesByQuery(value).then((data) => {
         createGallery(data.hits)
-    }).catch((error) =>{
-        console.log(error);
     }).finally(() => {
         hideLoader();
         event.target.reset();
